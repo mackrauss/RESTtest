2be7a929ebe16fa17bd6909eee9c09eff82be0ee
Armin Krauss <armin.krauss@utoronto.ca>
2012-05-12 16:57:56 -0400

    Checking network state before REST call is attempted, displaying result of call in text box

--- src/org/encorelab/RESTtestActivity.java
+++ src/org/encorelab/RESTtestActivity.java
@@ -12,60 +12,80 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class RESTtestActivity extends Activity {
+	private TextView textView1;
+	private EditText editText1;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        textView1 = (TextView) findViewById(R.id.textView1);
+        editText1 = (EditText) findViewById(R.id.editText1);
     }
     
     /** Called when the user selects the Send button */
     public void sendReq(View view) {
-        // Do something in response to button
-    	//Intent intent = new Intent(this, RequestTask.class);
-    	EditText editText1 = (EditText) findViewById(R.id.editText1);
-    	new RequestTask().execute(editText1.getText().toString());
+    	ConnectivityManager connMgr = (ConnectivityManager) 
+    	        getSystemService(Context.CONNECTIVITY_SERVICE);
+    	    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+    	    
+	    if (networkInfo != null && networkInfo.isConnected()) {
+	        // fetch data
+	    	new RequestTask().execute(editText1.getText().toString());
+	    } else {
+	        // display error
+	    	textView1.setText("No network connection active");
+	    } 
     }
-}
+    
+    private class RequestTask extends AsyncTask<String, String, String>{
 
-class RequestTask extends AsyncTask<String, String, String>{
-
-    @Override
-    protected String doInBackground(String... uri) {
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpResponse response;
-        String responseString = null;
-        try {
-            response = httpclient.execute(new HttpGet(uri[0]));
-            StatusLine statusLine = response.getStatusLine();
-            if(statusLine.getStatusCode() == HttpStatus.SC_OK){
-                ByteArrayOutputStream out = new ByteArrayOutputStream();
-                response.getEntity().writeTo(out);
-                out.close();
-                responseString = out.toString();
-            } else{
-                //Closes the connection.
-                response.getEntity().getContent().close();
-                throw new IOException(statusLine.getReasonPhrase());
+        @Override
+        protected String doInBackground(String... uri) {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpResponse response;
+            String responseString = null;
+            try {
+                response = httpclient.execute(new HttpGet(uri[0]));
+                StatusLine statusLine = response.getStatusLine();
+                if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+                    ByteArrayOutputStream out = new ByteArrayOutputStream();
+                    response.getEntity().writeTo(out);
+                    out.close();
+                    responseString = out.toString();
+                } else{
+                    //Closes the connection.
+                    response.getEntity().getContent().close();
+                    throw new IOException(statusLine.getReasonPhrase());
+                }
+            } catch (ClientProtocolException e) {
+                //TODO Handle problems..
+            } catch (IOException e) {
+                //TODO Handle problems..
             }
-        } catch (ClientProtocolException e) {
-            //TODO Handle problems..
-        } catch (IOException e) {
-            //TODO Handle problems..
+            return responseString;
         }
-        return responseString;
-    }
 
-    @Override
-    protected void onPostExecute(String result) {
-        super.onPostExecute(result);
-        //Do anything with response..
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            //Do anything with response..
+            textView1.setText(result);
+
+        }
     }
 }
+
+
